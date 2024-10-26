a13873b58a734783fce40139a26cfbbb53826d65
Andrew <andrew@redd.org>
2024-10-26 13:34:39 -0600

    Created a startup-react subfolder and copied startup-css into it then placed the deploy files from simon-react into it

---
+++ startup-react/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Function to toggle the navigation menu
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+}
