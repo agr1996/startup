dcb3955bf2e7d51371f42b4115f96cf3173aa55e
Andrew <andrew@redd.org>
2024-11-05 16:27:31 -0700

    not sure what i'm doing wrong

--- startup-react/src/App.jsx
+++ startup-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
@@ -8,7 +8,6 @@ import Share from './Share/Share'
 import SignIn from './SignIn/SignIn'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
   <Routes>
