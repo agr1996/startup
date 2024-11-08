3ae6b1d109e8fab6cda8078a0f728f825b53d685
Andrew <andrew@redd.org>
2024-11-08 11:51:28 -0700

    sign in page is mostly working

--- startup-react/src/signin/signin.jsx
+++ startup-react/src/signin/signin.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './SignIn.css'
 
 
 function SignIn() {
-    return <h1>Sign In</h1>
+    return (
+        <>
+            <form method="get" action="markup.html">
+                <div><h2>Sign in</h2></div>
+                <div>
+                    <label for="username"><small>Username </small></label>
+                    <input type="text" id="username" name="username" required></input>
+                </div>
+                <div>
+                    <label for="password"><small>Password </small></label>
+                    <input type="password" id="password" name="password" required></input>
+                </div>
+                <div>
+                    <input type="checkbox" id="rememberMe" name="rememberMe"></input>
+                    <label for="rememberMe"><small>Remember me</small></label>
+                </div>
+                <div>
+                    <button type="submit"><small>Sign in</small></button>
+                </div>
+            </form>
+        </>
+    );
 }
 
 export default SignIn;
