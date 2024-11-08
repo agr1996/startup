ec453859fe035c209654cbf3db1a273391c6d33f
Andrew <andrew@redd.org>
2024-11-08 12:28:40 -0700

    added and moved assets

--- startup-react/src/share/share.jsx
+++ startup-react/src/share/share.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 
 function Share() {
-    return <h1>Share</h1>
+    return (
+        <>
+            <form method="get" action="markup.html">
+                <div>
+                    <h2>Share</h2>
+                </div>
+                <div>
+                    <label for="id"><small>ID</small></label>
+                    <br></br>
+                    <input type="text" placeholder="Recipient ID" id="title" name="title" required></input>
+                </div>
+                <div>
+                    <label for="file"><small>File</small></label>
+                    <br></br>
+                    <input type="file" id="file" name="file" required></input>
+                </div>
+                <div>
+                    <label for="description"><small>Description</small></label>
+                    <br></br>
+                    <textarea id="description" placeholder="Optional" name="description"></textarea>
+                </div>
+                <div>
+                    <button type="submit">
+                        <img src='share.svg' alt="Sent" style="width: 20px; height: 20px;"></img>
+                    </button>
+                </div>
+            </form>
+        </>
+    );
 }
 
 export default Share;
