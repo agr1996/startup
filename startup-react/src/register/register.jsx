f92e0f380d6fbb992ee3214d0b0a8ed4f0cdef19
Andrew <andrew@redd.org>
2024-11-08 12:04:11 -0700

    register and signin have style pages as well as some functionality

--- startup-react/src/register/register.jsx
+++ startup-react/src/register/register.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
+
 function Register() {
-    return <h1>Register</h1>
+    return (
+        <>
+            <form method="get" action="markup.html">
+                <h2>Create Account</h2>
+                <div>
+                    <label for="username"><small>Username</small></label>
+                    <br></br>
+                    <input type="text" id="username" name="username" required></input>
+                </div>
+                <div>
+                    <label for="email"><small>Email</small></label>
+                    <br></br>
+                    <input type="email" id="email" name="email" required></input>
+                </div>
+                <div>
+                    <label for="password"><small>Password</small></label>
+                    <br></br>
+                    <input type="password" id="password" name="password" required></input>
+                </div>
+                <div>
+                    <label for="confirm-password"><small>Confirm Password</small></label>
+                    <br></br>
+                    <input type="password" id="confirm-password" name="confirm-password" required></input>
+                </div>
+                <div>
+                    <button type="submit"><small>Submit</small></button>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default Register;
--- startup-react/src/signin/signin.jsx
+++ startup-react/src/signin/signin.jsx
@@ -6,13 +6,17 @@ function SignIn() {
     return (
         <>
             <form method="get" action="markup.html">
-                <div><h2>Sign in</h2></div>
                 <div>
-                    <label for="username"><small>Username </small></label>
+                    <h2>Sign in</h2>
+                </div>
+                <div>
+                    <label for="username"><small>Username</small></label>
+                    <br></br>
                     <input type="text" id="username" name="username" required></input>
                 </div>
                 <div>
-                    <label for="password"><small>Password </small></label>
+                    <label for="password"><small>Password</small></label>
+                    <br></br>
                     <input type="password" id="password" name="password" required></input>
                 </div>
                 <div>
