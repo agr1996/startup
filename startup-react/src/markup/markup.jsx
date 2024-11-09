b3721d1c17f4aaca0ff759e700b6ec148c407c7f
Andrew <andrew@redd.org>
2024-11-08 19:33:59 -0700

    console has file

--- startup-react/src/markup/markup.jsx
+++ startup-react/src/markup/markup.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 
 function Markup() {
+    const [file, setFile] = useState(null);
 
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
 
+    const handleFileUpload = () => {
+        if (file) {
+            // Process the file here
+            console.log('File uploaded:', file);
+            // You can read the file content using FileReader or send it to a server
+        }
+    };
 
-
-    return <h1>Markup</h1>
+    return (
+        <>
+            <h1>Markup</h1>
+            <div>
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={handleFileUpload}>Upload</button>
+            </div>
+        </>
+    );
 }
 
 export default Markup;
