3858d447e1c7886af8077a05d363cfeb447a9320
Andrew <andrew@redd.org>
2024-11-08 19:58:08 -0700

    made the github link a button with the github logo

--- startup-react/src/Footer/Footer.jsx
+++ startup-react/src/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Footer.css";
+import github from './github.svg';
 
 function Footer() {
+
+    const handleClick = () => {
+        window.open('https://github.com/agr1996/startup', '_blank');
+    };
     return (
         <>
             <footer className="footer">
                 <strong>Andrew Redd</strong>
-                <br />
-                <a href="https://github.com/agr1996/startup">GitHub</a>
+                <button onClick={handleClick}>
+                <img src={github} alt="Sent" style={{ width: '20px', height: '20px' }} />
+                </button>
             </footer>
         </>
     );
