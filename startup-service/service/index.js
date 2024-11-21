4b37b42fcb7f4ff847febc2c34f3019a144e0509
Andrew <andrew@redd.org>
2024-11-20 18:25:17 -0700

    added express into the server

--- startup-service/service/index.js
+++ startup-service/service/index.js
@@ -1,10 +1,7 @@
-const http = require('http');
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write(`<h1>Hello Node.js! [${req.method}] ${req.url}</h1>`);
-  res.end();
-});
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
 
-server.listen(8080, () => {
-  console.log(`Web service listening on port 8080`);
-});
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.static('public'));
