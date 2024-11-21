9555fe78b0192d9f60762b624d7f4a6bf5ddea73
Andrew <andrew@redd.org>
2024-11-20 18:39:36 -0700

    Put stuff back in

--- startup-service/service/index.js
+++ startup-service/service/index.js
@@ -3,4 +3,9 @@ const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
