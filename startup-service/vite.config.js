4be488c092d255831ee0257cf201ef7a4ef86c98
Andrew <andrew@redd.org>
2024-11-20 18:05:01 -0700

    trying to get it working

--- startup-service/vite.config.js
+++ startup-service/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
-    },
-  },
-});
+  plugins: [react()],
+})
