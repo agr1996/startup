ea5f8d34b18f0c7868183bd4afdfce8e842a7724
Andrew <andrew@redd.org>
2024-12-11 14:10:09 -0700

    removed some functions from back end

--- startup-service/service/index.js
+++ startup-service/service/index.js
@@ -4,7 +4,6 @@ const app = express();
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = {};
-let scores = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -60,45 +59,3 @@ apiRouter.delete('/auth/logout', (req, res) => {
   res.status(204).end();
 });
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  console.log("in Scored");
-  res.send(scores);
-});
-
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
-});
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-// updateScores considers a new score for inclusion in the high scores.
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScore);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}
